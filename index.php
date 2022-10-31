e4f04450f742284726483fd28a274f03a809e805
Bennie Robinson <brobinson88@gmail.com>
2022-10-31 12:20:12 -0400

    Sanitize input

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'functions.php';
 connect();
 
 if ( isXHR() && isset($_POST['q']) ) {
-  echo json_encode( get_artists( $_POST['q'] ) ); 
+  echo json_encode( get_artists( $_POST['q'] ) );
   return;
 }
 
@@ -14,7 +14,7 @@ if ( isset($_POST['q']) ) {
 
 if ( isXHR() && isset( $_POST['album_id'] ) ) {
   $info = get_album_rating( $_POST['album_id'] );
-  echo $info->Rating;
+  echo json_encode($info->Rating);
   return;
 }
 
